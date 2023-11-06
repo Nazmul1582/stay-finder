8d04fab14911e306524da3316e71d6faa77dc4fd
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-06 15:54:50 +0600

    created user create functionality and sent utility to all components

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,14 +1,46 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
+const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const authInfo = {}
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        return unsubscribe;
+    }, [])
+
+    const createUser = async(email, password, username) => {
+        
+        // create user
+        await createUserWithEmailAndPassword(auth, email, password)
+
+        // update profile
+        await updateProfile(auth.currentUser, {displayName: username})
+
+        // set user
+        const user = auth.currentUser;
+        setCurrentUser({
+            ...user
+        })
+    }
+
+    const authInfo = {
+        currentUser, 
+        loading, 
+        createUser
+    }
 
     return (
         <AuthContext.Provider value={authInfo}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     );
 };
