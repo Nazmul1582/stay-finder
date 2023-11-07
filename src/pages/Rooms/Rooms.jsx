e6ff5b0db061ebae1b6f26bb83088180e829cc4c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-07 18:52:00 +0600

    created a serch bar for filter

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,11 +1,29 @@
 import { Link, useLoaderData } from "react-router-dom";
 
 const Rooms = () => {
-  const rooms = useLoaderData()
+  const rooms = useLoaderData();
+  const handleFilter = event => {
+    const input = event.target.value;
+    console.log(input);
+  }
 
   return (
     <section className="py-20">
       <div className="container mx-auto">
+
+        <div className="form-control mb-20">
+          <div className="input-group items-center justify-center">
+            <select onChange={handleFilter} className="select select-bordered focus:outline-0 w-full md:w-1/2">
+              <option disabled selected>
+                Filter by price
+              </option>
+              <option value="asc">low to height</option>
+              <option value="desc">height to low</option>
+            </select>
+            <button className="btn">Filter</button>
+          </div>
+        </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {rooms.map((room) => (
             <div key={room._id} className="card bg-base-100 shadow-xl">
