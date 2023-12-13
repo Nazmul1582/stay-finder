6071616af0773b5ddb2aa1c43d3921c8d6868a0f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-13 18:04:56 +0600

    intalled react-facebook and implemented messenger chat bot

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../pages/shared/Header";
 import Footer from "../pages/shared/Footer";
+import { CustomChat, FacebookProvider } from "react-facebook";
 
 const Main = () => {
-    return (
-        <div className="font-inter">
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="font-inter">
+      <Header />
+      <Outlet />
+      <Footer />
+      <FacebookProvider appId="315102574689798" chatSupport>
+        <CustomChat pageId="198124730045267" minimized={true} />
+      </FacebookProvider>
+    </div>
+  );
 };
 
 export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
@@ -9,9 +8,7 @@ import AuthProvider from './Provider/AuthProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>,
 )
